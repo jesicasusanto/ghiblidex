55b81fc5ac4e410c66e196766c13efceea3c309b
jesicasusanto <jesica.ssnt@gmail.com>
2025-04-03 21:33:39 -0400

    Add Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 import { useState } from 'react';
 import './App.css';
+import Search from './components/Search';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');
 
   return (
     <main>
       <div className="flex relative justify-center flex-col items-center">
         <div className="relative w-max">
-          <h2 className="font-title font-bold text-[min(10vw,128px)] text-[#7B4600] relative">
+          <h2 className="font-title font-bold text-[min(14vw,128px)] text-brownie relative">
             GhibliDex
-            {/* Leaf positioned relative to the text */}
             <img
-              className="absolute top-[-2%] left-[-5%] h-[min(7vw,85px)] w-[min(7vw,79px)]"
+              className="absolute top-[-2%] left-[-8%] h-[min(10vw,85px)] w-[min(10vw,79px)]"
               src="/leaf.png"
               alt="leaf"
             />
           </h2>
         </div>
-        <h3 className="font-playpen text-[min(2vw,24px)] text-center">
+        <h3 className="font-playpen text-[min(4vw,24px)] text-center">
           Ghibli anime search engine
         </h3>
       </div>
+
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
     </main>
   );
 }
