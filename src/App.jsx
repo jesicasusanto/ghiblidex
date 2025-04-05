e18172ea6333f0895943cd5cd7554c33d799d1ba
jesicasusanto <jesica.ssnt@gmail.com>
2025-04-05 07:47:14 -0400

    Add search api

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,47 @@ import { useState } from 'react';
 import './App.css';
 import Search from './components/Search';
 
-function App() {
+const API_BASE_URL = 'https://ghibliapi.vercel.app';
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+  }
+}
+
+const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
+  const [errorMessage, setErrorMessage] = useState('');
+  const fetchMovies = async () => {
+    try {
+
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage("Error fetching movies. Please try again later.");
+    }
+  }
   return (
     <main>
-      <div className="flex relative justify-center flex-col items-center">
-        <div className="relative w-max">
-          <h2 className="font-title font-bold text-[min(14vw,128px)] text-brownie relative">
-            GhibliDex
-            <img
-              className="absolute top-[-2%] left-[-8%] h-[min(10vw,85px)] w-[min(10vw,79px)]"
-              src="/leaf.png"
-              alt="leaf"
-            />
-          </h2>
+      <header>
+        <div className="flex relative justify-center flex-col items-center">
+          <div className="relative w-max">
+            <h2 className="font-title font-bold text-[min(14vw,128px)] text-brownie relative">
+              GhibliDex
+              <img
+                className="absolute top-[-2%] left-[-8%] h-[min(10vw,85px)] w-[min(10vw,79px)]"
+                src="/leaf.png"
+                alt="leaf"
+              />
+            </h2>
+          </div>
+          <h3 className="font-playpen text-[min(4vw,24px)] text-center">
+            Studio Ghibli anime search engine
+          </h3>
         </div>
-        <h3 className="font-playpen text-[min(4vw,24px)] text-center">
-          Ghibli anime search engine
-        </h3>
-      </div>
-
-      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      </header>
     </main>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Search = ({ searchTerm, setSearchTerm }) => {
   return (
-    <div className="flex items-center bg-white text-black opacity-80 rounded-xl border-brownie border-2 font-playpen my-4 px-2 drop-shadow-xl">
+    <div className="flex items-center bg-white text-black opacity-80 rounded-3xl border-brownie border-2 font-playpen my-4 px-2 drop-shadow-xl">
       <img 
         src="magnifier.svg" 
         alt="search" 
-        className="w-[max(4vw,6px)] h-[max(4vh,6px)]mx-2" // Responsive image sizes
+        className="w-[max(3vw,5px)] h-[max(3vh,5px)]mx-2 my-2"
       />
       <input 
         type="text" 
         placeholder="Type your search here" 
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        className="flex-grow p-2 rounded-r-xl text-[min(4vw,24px)]" // Responsive input size
+        className="flex-grow p-2 rounded-r-xl text-[min(4vw,24px)]"
       />
     </div>
   )
